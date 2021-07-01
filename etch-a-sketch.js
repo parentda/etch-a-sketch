67058e828221437de22c3ec5e95159ea7a4ebd91
parentda <danielparent21@gmail.com>
2021-07-01 15:27:41 -0400

    Added grid initialization and made grid re-drawing dynamic

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,12 +1,30 @@
-const sketchContainer = document.querySelector("#sketch-container");
-console.log(sketchContainer);
+const root = document.documentElement;
 
-function drawGrid(gridSize) {
-  for (let i = 0; i < gridSize ** 2; i++) {
+const sketchContainer = document.querySelector("#sketch-container");
+const gridSizeInput = document.querySelector("#grid-size input");
+const gridSizeDisplay = document.querySelector("#grid-size p");
+
+// Sketch area initialization
+document.addEventListener("DOMContentLoaded", drawGrid);
+gridSizeDisplay.textContent = gridSizeInput.value;
+
+gridSizeInput.addEventListener("input", (e) => {
+  gridSizeDisplay.textContent = e.target.value;
+});
+
+gridSizeInput.addEventListener("change", drawGrid);
+
+function drawGrid() {
+  const gridSize = gridSizeInput.value;
+  root.style.setProperty("--grid-size", gridSize);
+
+  while (sketchContainer.firstChild) {
+    sketchContainer.removeChild(sketchContainer.firstChild);
+  }
+
+  for (let i = 0; i < gridSize ** 2; i += 1) {
     const gridBox = document.createElement("div");
     gridBox.classList.add("grid-box");
     sketchContainer.appendChild(gridBox);
   }
 }
-
-drawGrid(100);
