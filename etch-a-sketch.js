7ca9abeca14fc3da8f0261593ac0ce66075daa52
parentda <danielparent21@gmail.com>
2021-07-01 21:22:51 -0400

    Added drawing functionality that requires both click and hover

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -4,15 +4,31 @@ const sketchContainer = document.querySelector("#sketch-container");
 const gridSizeInput = document.querySelector("#grid-size input");
 const gridSizeDisplay = document.querySelector("#grid-size p");
 
+let mouseDown = false;
+
+window.addEventListener("pointerdown", () => {
+  mouseDown = true;
+});
+
+window.addEventListener("pointerup", () => {
+  mouseDown = false;
+});
+
 // Sketch area initialization
-document.addEventListener("DOMContentLoaded", drawGrid);
+window.addEventListener("DOMContentLoaded", drawGrid);
 gridSizeDisplay.textContent = gridSizeInput.value;
 
-gridSizeInput.addEventListener("input", (e) => {
-  gridSizeDisplay.textContent = e.target.value;
+gridSizeInput.addEventListener("input", () => {
+  gridSizeDisplay.textContent = gridSizeInput.value;
 });
 
 gridSizeInput.addEventListener("change", drawGrid);
+
+function changeColor(event) {
+  if (mouseDown) {
+    event.target.style.backgroundColor = "black";
+  }
+}
 
 function drawGrid() {
   const gridSize = gridSizeInput.value;
@@ -25,6 +41,7 @@ function drawGrid() {
   for (let i = 0; i < gridSize ** 2; i += 1) {
     const gridBox = document.createElement("div");
     gridBox.classList.add("grid-box");
+    gridBox.addEventListener("mouseover", changeColor);
     sketchContainer.appendChild(gridBox);
   }
 }
