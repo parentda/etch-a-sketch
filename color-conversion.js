9ba022d4ce378c66facf354e97cee42780f2664b
parentda <danielparent21@gmail.com>
2021-07-04 16:06:44 -0400

    Added hextoHSL to color-conversion.js

--- color-conversion.js
+++ color-conversion.js
@@ -9,20 +9,20 @@ function parseHSL(hslString) {
   return [h, s, l];
 }
 
-function RGBToHSL(rgb) {
-  let sep = rgb.indexOf(",") > -1 ? "," : " ";
-  rgb = rgb.substr(4).split(")")[0].split(sep);
+function RGBToHSL(rgbString) {
+  let sep = rgbString.indexOf(",") > -1 ? "," : " ";
+  rgbString = rgbString.substr(4).split(")")[0].split(sep);
 
-  for (let R in rgb) {
-    let r = rgb[R];
+  for (let R in rgbString) {
+    let r = rgbString[R];
     if (r.indexOf("%") > -1)
-      rgb[R] = Math.round((r.substr(0, r.length - 1) / 100) * 255);
+      rgbString[R] = Math.round((r.substr(0, r.length - 1) / 100) * 255);
   }
 
   // Make r, g, and b fractions of 1
-  let r = rgb[0] / 255,
-    g = rgb[1] / 255,
-    b = rgb[2] / 255;
+  let r = rgbString[0] / 255,
+    g = rgbString[1] / 255,
+    b = rgbString[2] / 255;
 
   // Find greatest and smallest channel values
   let cmin = Math.min(r, g, b),
@@ -54,6 +54,48 @@ function RGBToHSL(rgb) {
   s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
 
   // Multiply l and s by 100
+  s = +(s * 100).toFixed(1);
+  l = +(l * 100).toFixed(1);
+
+  return `hsl(${h}, ${s}%, ${l}%)`;
+}
+
+function hexToHSL(H) {
+  // Convert hex to RGB first
+  let r = 0,
+    g = 0,
+    b = 0;
+  if (H.length == 4) {
+    r = "0x" + H[1] + H[1];
+    g = "0x" + H[2] + H[2];
+    b = "0x" + H[3] + H[3];
+  } else if (H.length == 7) {
+    r = "0x" + H[1] + H[2];
+    g = "0x" + H[3] + H[4];
+    b = "0x" + H[5] + H[6];
+  }
+  // Then to HSL
+  r /= 255;
+  g /= 255;
+  b /= 255;
+  let cmin = Math.min(r, g, b),
+    cmax = Math.max(r, g, b),
+    delta = cmax - cmin,
+    h = 0,
+    s = 0,
+    l = 0;
+
+  if (delta == 0) h = 0;
+  else if (cmax == r) h = ((g - b) / delta) % 6;
+  else if (cmax == g) h = (b - r) / delta + 2;
+  else h = (r - g) / delta + 4;
+
+  h = Math.round(h * 60);
+
+  if (h < 0) h += 360;
+
+  l = (cmax + cmin) / 2;
+  s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
   s = +(s * 100).toFixed(1);
   l = +(l * 100).toFixed(1);
 
